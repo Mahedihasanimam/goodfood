c5858b5935a48a7d92ae200dda2331b246034da8
mahedi066 <imam25349@gmail.com>
2024-07-12 02:35:04 +0600

    FAQ section added

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import WhySimplyGoodFood from "./whygoodfood/page";
 import TrandingMen from "./components/TrandingMen";
 import Dailymela from "./components/Dailymela";
+import FaqSection from "./components/FaqSection";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <WhySimplyGoodFood/>
       <TrandingMen/>
       <Dailymela/>
+      <FaqSection/>
     </main>
   );
 }
